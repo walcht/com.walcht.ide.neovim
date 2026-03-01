aa3e4df69bf80594ebd53ccfdf5c26060211ed3a
Andrei <COPATHuK@gmail.com>
2026-03-02 01:27:00 +0700

    feat: add TabView structure with 4 tab placeholders

--- Editor/NeovimSettingsWindow.cs
+++ Editor/NeovimSettingsWindow.cs
@@ -20,7 +20,54 @@ namespace Neovim.Editor
 
     public void CreateGUI()
     {
-      // TabView will be added here
+      var tabView = new TabView();
+      tabView.style.flexGrow = 1;
+
+      // Tab 1: Behavior
+      var behaviorTab = new VisualElement();
+      behaviorTab.name = "BehaviorTab";
+      CreateBehaviorTab(behaviorTab);
+      tabView.AddTab("Behavior", behaviorTab);
+
+      // Tab 2: Terminal
+      var terminalTab = new VisualElement();
+      terminalTab.name = "TerminalTab";
+      CreateTerminalTab(terminalTab);
+      tabView.AddTab("Terminal", terminalTab);
+
+      // Tab 3: File Opening
+      var fileOpeningTab = new VisualElement();
+      fileOpeningTab.name = "FileOpeningTab";
+      CreateFileOpeningTab(fileOpeningTab);
+      tabView.AddTab("File Opening", fileOpeningTab);
+
+      // Tab 4: Maintenance
+      var maintenanceTab = new VisualElement();
+      maintenanceTab.name = "MaintenanceTab";
+      CreateMaintenanceTab(maintenanceTab);
+      tabView.AddTab("Maintenance", maintenanceTab);
+
+      rootVisualElement.Add(tabView);
+    }
+
+    private void CreateBehaviorTab(VisualElement container)
+    {
+      // Will be implemented in Task 3
+    }
+
+    private void CreateTerminalTab(VisualElement container)
+    {
+      // Will be implemented in Task 4
+    }
+
+    private void CreateFileOpeningTab(VisualElement container)
+    {
+      // Will be implemented in Task 5
+    }
+
+    private void CreateMaintenanceTab(VisualElement container)
+    {
+      // Will be implemented in Task 6
     }
   }
 }
